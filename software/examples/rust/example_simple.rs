79c8028f465e79afe1c1dee31a53f7647ab82f33
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:22:25 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{industrial_dual_0_20ma_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_dual_0_20ma_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual 0-20mA Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual 0-20mA Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_dual_0_20ma_v2_bricklet = IndustrialDual020mAV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let id020 = IndustrialDual020maV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current current from channel 0
-    let voltage = industrial_dual_0_20ma_v2_bricklet.get_current(0).recv()?;
-    println!("Voltage (Channel 0): {}{}", voltage as f32 / 1000000.0, " mA");
+    // Get current current from channel 0.
+    let voltage = id020.get_current(0).recv()?;
+    println!("Voltage (Channel 0): {} mA", voltage as f32 / 1000000.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,32 +1,33 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{industrial_dual_0_20ma_v2_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{industrial_dual_0_20ma_v2_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual 0-20mA Bricklet 2.0
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Industrial Dual 0-20mA Bricklet 2.0.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let industrial_dual_0_20ma_v2_bricklet = IndustrialDual020mAV2Bricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let id020 = IndustrialDual020maV2Bricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for voltage events.
-    let current_listener = industrial_dual_0_20ma_v2_bricklet.get_current_receiver();
-    // Spawn thread to handle received events. This thread ends when the industrial_dual_0_20ma_v2_bricklet
+    // Create receiver for voltage events.
+    let current_receiver = id020.get_current_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `id020` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in current_listener {
-            println!("Channel: {}", event.channel);
-            println!("Current: {}{}", event.current as f32 / 1000000.0, " mA");
+        for current in current_receiver {
+            println!("Channel: {}", current.channel);
+            println!("Current: {} mA", current.current as f32 / 1000000.0);
             println!();
         }
     });
 
     // Configure threshold for current (channel 0) "greater than 10 mA"
-    // with a debounce period of 10s (10000ms)
-    industrial_dual_0_20ma_v2_bricklet.set_current_callback_configuration(0, 10000, false, '>', 10 * 1000000, 0);
+    // with a debounce period of 10s (10000ms).
+    id020.set_current_callback_configuration(0, 10000, false, '>', 10 * 1000000, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
